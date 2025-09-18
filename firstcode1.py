6bc5700f479f1ed19a8f0474d3bbf518d5a2d5d4
ankita21-tech <gadhavea368@gmail.com>
2025-09-18 18:27:27 +0530

    first file

---
+++ firstcode1.py
@@ -0,0 +1,4 @@
+"Hello World" 
+print("Hello World")
+# This is a simple Python program that prints "Hello World" to the console.
+# It serves as an introductory example for beginners learning Python programming.
