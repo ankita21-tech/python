210010a39a264c540dfcbd26da016090fb960c09
Ankita Gadhave <gadhavea368@gmail.com>
2025-09-18 18:28:58 +0530

    Update firstcode1.py

--- firstcode1.py
+++ firstcode1.py
@@ -1,4 +1,3 @@
 "Hello World" 
 print("Hello World")
 # This is a simple Python program that prints "Hello World" to the console.
-# It serves as an introductory example for beginners learning Python programming.
